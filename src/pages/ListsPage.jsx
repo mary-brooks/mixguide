49685def86144cc9b96d5888c8a198e3f40511e6
Brenda Silveira <brendasilveira.2010@hotmail.com>
2024-01-10 21:43:25 +0000

    displayed list cards

--- src/pages/ListsPage.jsx
+++ src/pages/ListsPage.jsx
@@ -1,5 +1,45 @@
+import Navbar from '../components/Navbar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 function ListsPage() {
-  return <div>ListsPage</div>;
-}
+
+  const [lists, setLists] = useState([])
+
+  const API_URL = 'https://cocktail-app-mock-backend.adaptable.app/lists';
+
+  const getLists = async () => {
+    try {
+      const response = await axios.get(`${API_URL}`);
+      setLists(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLists();
+  }, []);
+
+
+  return (
+    <div>
+      <Navbar />
+      <div className='lists-container'>
+        {lists &&
+          lists.map(list => {
+            return (
+                <Link key={list.id}>
+                  <div className='list-card'>
+                    <h2>{list.title}</h2>
+                  </div>
+                </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
+  }
 
 export default ListsPage;
