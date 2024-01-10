b7e5fbd50b0b9d831e6f4b6b661d440b737d3e57
Brenda Silveira <brendasilveira.2010@hotmail.com>
2024-01-10 21:26:13 +0000

    created roots for lists

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Lists from './pages/ListsPage'
 import CocktailOfTheDay from './pages/CocktailOfTheDayPage'
 import SingleCocktail from './pages/SingleCocktail';
 import SearchCocktails from './pages/SearchCocktailsPage'
+import AddList from './pages/AddListPage';
+import SingleList from './pages/SingleList';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/cocktails" element={<SearchCocktails />} />
       <Route path="/lists" element={<Lists />} />
+      <Route path="/lists/:listId" element={<SingleList />} />
+      <Route path='/lists/addlist' element={<AddList />} />
       <Route path="/cocktailoftheday" element={<CocktailOfTheDay />} />
       <Route path="/cocktails/:cocktailId" element={<SingleCocktail />} />
     </Routes>
---
+++ src/pages/AddListPage.jsx
@@ -0,0 +1,7 @@
+function AddListPage() {
+  return (
+    <div>AddListPage</div>
+  )
+}
+
+export default AddListPage
