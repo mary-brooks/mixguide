c67db2db7583ef20a4da64402c39d425b400e5b3
mary-brooks <maryemmabrooks@gmail.com>
2024-01-13 09:51:26 +0000

    displayed single list details

--- src/pages/SingleList.jsx
+++ src/pages/SingleList.jsx
@@ -1,13 +1,13 @@
 import Navbar from '../components/Navbar';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function SingleList() {
+  const { listId } = useParams();
 
-  const {listId} = useParams();
-
-  const [singleList, setSingleList] = useState(null)
+  const [singleList, setSingleList] = useState(null);
 
   const API_URL = `https://cocktail-app-mock-backend.adaptable.app/lists/${listId}`;
 
@@ -15,6 +15,7 @@ function SingleList() {
     try {
       const response = await axios.get(`${API_URL}`);
       setSingleList(response.data);
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -26,15 +27,26 @@ function SingleList() {
 
   return (
     <div>
-     <Navbar />
-{/*       singleList && (
+      <Navbar />
+
+      {singleList && (
         <div className='single-list'>
           <h2>{singleList.title}</h2>
-          <h3>{singleList.created_by}</h3>
+          <h3>Contributed by: {singleList.created_by}</h3>
+          {singleList.cocktails.map(cocktail => {
+            return (
+              <Link key={cocktail.id}>
+                <div className='recipe-card'>
+                  <img src={cocktail.image_url} alt={cocktail.recipe_title} />
+                  <h2>{cocktail.recipe_title}</h2>
+                </div>
+              </Link>
+            );
+          })}
         </div>
-      ) */}
+      )}
     </div>
-  )
+  );
 }
 
-export default SingleList
+export default SingleList;
