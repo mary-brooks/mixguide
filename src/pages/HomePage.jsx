519bfd5cdca758fbd15523337c30a7b162dd1972
Brenda Silveira <brendasilveira.2010@hotmail.com>
2024-01-19 15:36:20 +0000

    added styling to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/images/logo2.png';
+import starLeft from '../assets/images/star-icon-left.png';
+import starRight from '../assets/images/star-icon-right.png';
+import background from '../assets/images/background-img.png';
+import 'animate.css';
 
 function HomePage() {
   return (
-   <div className="home-page">
-    <Link className="home-page-link" to='/cocktails'><button>Get started</button></Link>
-    <Link className="home-page-link" to='/cocktailoftheday'><button>Cocktail of the day</button></Link>
-  </div>
-  )
+    <div className="home-page" style={{ backgroundImage: `url(${background})` }}>
+
+        <img className="star-right animate__animated animate__rollIn" src={starRight} alt='star-icon' />
+
+      <img className="logo animate__animated animate__fadeInUp" src={logo} alt='mix-guide-logo' />
+
+        <img className="star-left animate__animated animate__rollIn" src={starLeft} alt='star-icon' />
+
+      <Link className="get-started-btn animate__animated animate__fadeInDown animate__delay-1s" to='/cocktails'><button>Get Started</button></Link>
+
+      <div className="hp-buttons-container animate__animated animate__fadeInDown animate__delay-2s">
+        <Link className="cocktail-day-btn" to='/cocktailoftheday'><button>Cocktail of the day</button></Link>
+        <p>|</p>
+        <Link className="all-cocktails-btn"> <button>All cocktails</button></Link>
+      </div>
+
+    </div>
+  );
 }
 
 export default HomePage;
