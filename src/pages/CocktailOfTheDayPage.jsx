21f19c4f5081b6b495e4f0f4ec6a18ad2dfb8a8e
mary-brooks <maryemmabrooks@gmail.com>
2024-01-20 17:03:57 +0000

    added styles to single cocktail and cocktail of the day

--- src/pages/CocktailOfTheDayPage.jsx
+++ src/pages/CocktailOfTheDayPage.jsx
@@ -7,7 +7,9 @@ function CocktailOfTheDayPage() {
 
   const fetchCocktailOfTheDay = async () => {
     try {
-      const response = await fetch('https://cocktail-app-mock-backend.adaptable.app/cocktails');
+      const response = await fetch(
+        'https://cocktail-app-mock-backend.adaptable.app/cocktails'
+      );
       const data = await response.json();
 
       console.log('Fetched Data:', data);
@@ -18,7 +20,10 @@ function CocktailOfTheDayPage() {
 
         console.log('Selected Cocktail:', randomCocktail);
 
-        localStorage.setItem('cocktailOfTheDay', JSON.stringify(randomCocktail));
+        localStorage.setItem(
+          'cocktailOfTheDay',
+          JSON.stringify(randomCocktail)
+        );
         setCocktailOfTheDay(randomCocktail);
       } else {
         console.error('No valid data received.');
@@ -53,48 +58,54 @@ function CocktailOfTheDayPage() {
 
   console.log('cocktailOfTheDay:', cocktailOfTheDay);
 
-
   return (
     <div>
       <Navbar />
       {cocktailOfTheDay && (
         <div className='cocktail-container'>
-        <div className='cocktail-details'>
-          <img src={cocktailOfTheDay.image} alt={`image of a ${cocktailOfTheDay.recipe_title}`} />
-          <div className='cocktail-ingredients'>
-            <h1>{cocktailOfTheDay.recipe_title}</h1>
-            <h2>Ingredients:</h2>
-            <ul>
-              {cocktailOfTheDay.ingredients.map((ingredient) => (
-                <li key={ingredient}>{ingredient}</li>
-              ))}
-            </ul>
-            <h2>Garnish:</h2>
-            <ul>
-              {cocktailOfTheDay.garnish.map((garnish) => (
-                <li key={garnish}>{garnish}</li>
-              ))}
-            </ul>
+          <div className='cocktail-details'>
+            <div className='image-container'>
+              <img
+                src={cocktailOfTheDay.image}
+                alt={`image of a ${cocktailOfTheDay.recipe_title}`}
+              />
+            </div>
+            <div className='cocktail-ingredients'>
+              <h1>{cocktailOfTheDay.recipe_title}</h1>
+              <h2>Ingredients:</h2>
+              <ul>
+                {cocktailOfTheDay.ingredients.map(ingredient => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
+              <h2>Garnish:</h2>
+              <ul>
+                {cocktailOfTheDay.garnish.map(garnish => (
+                  <li key={garnish}>{garnish}</li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className='cocktail-instructions'>
-          <h2>Instructions:</h2>
-          <ul>
-            {cocktailOfTheDay.instructions.map((instruction) => (
-              <li key={instruction}>{instruction}</li>
-            ))}
-          </ul>
-          <h3>Alcohol percentage:</h3>
-          <span>{cocktailOfTheDay.alcohol_percentage}</span>
-          <h3>Calories:</h3>
-          <span>{cocktailOfTheDay.calories}</span>
-          <h3>Rating:</h3>
-          <span>{cocktailOfTheDay.rating}</span>
-        </div>
+          <div className='cocktail-instructions'>
+            <h2>Instructions:</h2>
+            <ul>
+              {cocktailOfTheDay.instructions.map(instruction => (
+                <li key={instruction}>{instruction}</li>
+              ))}
+            </ul>
+            <h3>Alcohol percentage:</h3>
+            <span>{cocktailOfTheDay.alcohol_percentage}</span>
+            <h3>Calories:</h3>
+            <span>{cocktailOfTheDay.calories}</span>
+            <h3>Rating:</h3>
+            <span>{cocktailOfTheDay.rating}</span>
+          </div>
         </div>
       )}
 
-      <Link className='backToHome' to="/">Home</Link>
+      <Link className='backToHome' to='/'>
+        Home
+      </Link>
     </div>
   );
 }
--- src/pages/SingleCocktail.jsx
+++ src/pages/SingleCocktail.jsx
@@ -5,64 +5,74 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function CocktailDetails() {
+  const { cocktailId } = useParams();
 
-  const {cocktailId} = useParams()
-
-  const [cocktailDetails, setCocktailDetails] = useState(null)
+  const [cocktailDetails, setCocktailDetails] = useState(null);
 
   const getCocktailDetails = async () => {
     try {
       const response = await axios.get(
         `https://cocktail-app-mock-backend.adaptable.app/cocktails/${cocktailId}`
-      )
+      );
       setCocktailDetails(response.data);
     } catch (error) {
-        console.error('Error loading cocktail:', error)
+      console.error('Error loading cocktail:', error);
     }
-  }
+  };
 
   useEffect(() => {
-    getCocktailDetails()
-  }, [])
+    getCocktailDetails();
+  }, []);
 
   return (
     <div>
       <Navbar />
       {cocktailDetails && (
-        <div className='cocktail-details'>
-          <img src={cocktailDetails.image} alt={`image of a ${cocktailDetails.recipe_title}`} />
-          <h1>{cocktailDetails.recipe_title}</h1>
-          <h2>Ingredients:</h2>
-          <ul>
-            {cocktailDetails.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-          <h2>Garnish:</h2>
-          <ul>
-            {cocktailDetails.garnish.map((garnish) => (
-              <li key={garnish}>{garnish}</li>
-            ))}
-          </ul>
-          <h2>Instructions:</h2>
-          <ul>
-            {cocktailDetails.instructions.map((instruction) => (
-              <li key={instruction}>{instruction}</li>
-            ))}
-          </ul>
-          <h3>Alcohol percentage:</h3>
-          <span>{cocktailDetails.alcohol_percentage}</span>
-          <h3>Calories:</h3>
-          <span>{cocktailDetails.calories}</span>
-          <h3>Rating:</h3>
-          <span>{cocktailDetails.rating}</span>
+        <div className='cocktail-container'>
+          <div className='cocktail-details'>
+            <div className='image-container'>
+              <img
+                src={cocktailDetails.image}
+                alt={`image of a ${cocktailDetails.recipe_title}`}
+              />
+            </div>
+            <div className='cocktail-ingredients'>
+              <h1>{cocktailDetails.recipe_title}</h1>
+              <h2>Ingredients:</h2>
+              <ul>
+                {cocktailDetails.ingredients.map(ingredient => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
+              <h2>Garnish:</h2>
+              <ul>
+                {cocktailDetails.garnish.map(garnish => (
+                  <li key={garnish}>{garnish}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className='cocktail-instructions'>
+            <h2>Instructions:</h2>
+            <ul>
+              {cocktailDetails.instructions.map(instruction => (
+                <li key={instruction}>{instruction}</li>
+              ))}
+            </ul>
+            <h3>Alcohol percentage:</h3>
+            <span>{cocktailDetails.alcohol_percentage}</span>
+            <h3>Calories:</h3>
+            <span>{cocktailDetails.calories}</span>
+            <h3>Rating:</h3>
+            <span>{cocktailDetails.rating}</span>
+          </div>
         </div>
       )}
-
-      <Link className='backToHome' to="/">Home</Link>
+      <Link className='backToHome' to='/'>
+        Home
+      </Link>
     </div>
   );
-
 }
 
-export default CocktailDetails
+export default CocktailDetails;
